26220ce393414bfb12d86255b2dc70bd2946dc76
pevnostds <wizarddewa123@gmail.com>
2025-07-05 14:25:00 +0700

    updated vercel

--- models/index.js
+++ models/index.js
@@ -6,14 +6,22 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config, {
+    dialectModule: require('pg'),
+  })
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config,
+    { dialectModule: require('pg') },
+  )
 }
 
 fs
